74cbb937caa8e5417e941ca124b8cd03ffcda311
katniss邹洁 <katnisszoujie@MacBook-Pro.local>
2022-09-29 22:35:32 -0700

    first try

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 // Assignment code here
-
+var numric = "0123456789";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialcharacters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var passwordLength = 8;
+var password = "";
+var validChars = ""
 
 // Get references to the #generate element
+var password = document.getElementById("password");
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword () 
+if (password) {
+  validChars += numric;
+}
+
+if (password) {
+  validChars += lowercase;
+}
+
+if (password) {
+  validChars += uppercase;
+}
+
+if (password) {
+  validChars += specialcharacters;
+}
+
+function generatePassword() {
+  for (var i = 0; i < passwordLength; i++) {
+    var index = Math.floor(Math.random() * validChars.length);
+    password += validChars[index];
+  }
+
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
